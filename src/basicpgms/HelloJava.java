7839d885a3a7f216c531ac9963260602318c1282
Luminar Technolab <ashwathybasil@gmail.com>
2023-04-26 22:03:32 +0530

    initial commit

---
+++ src/basicpgms/HelloJava.java
@@ -0,0 +1,13 @@
+package basicpgms;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("java");
+		System.out.print("hello world");
+		System.out.print("java");
+
+	}
+
+}
